f6954426b7ca88367a84afbe515898338d470dc9
aliangya <2622404355@qq.com>
2022-08-09 09:19:38 +0800

    first commit

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,5 @@
+//document.createElement('div')
+const div = dom.create("<div><span>1</span></div>");
+console.log(div);
+
+//# sourceMappingURL=index.de158e3a.js.map
---
+++ src/dom.js
@@ -0,0 +1,34 @@
+window.dom = {
+    create(string) {
+        const container = document.createElement("div");
+        container.innerHTML = string;
+        return document.createElement[0];
+    }
+
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            // dom.style(div, 'color', 'red')
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                // dom.style(div, 'color')
+                return node.style[name]
+            } else if (name instanceof Object) {
+                // dom.style(div, {color: 'red'})
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key]
+                }
+            }
+        }
+    },
+
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    },
+};
